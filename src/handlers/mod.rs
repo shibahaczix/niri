9d37f44cd7454964d86ec7eda8f31ce683903688
urayde <urayde@ganyu.ru>
2026-04-20 11:59:26 +0300

    Implement `linux-drm-syncobj-v1` protocol
    `NIRI_DISABLE_SYNCOBJ` environment variable can be used to disable
    explicit sync.

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -29,6 +29,7 @@ use smithay::wayland::dmabuf::{DmabufGlobal, DmabufHandler, DmabufState, ImportN
 use smithay::wayland::drm_lease::{
     DrmLease, DrmLeaseBuilder, DrmLeaseHandler, DrmLeaseRequest, DrmLeaseState, LeaseRejected,
 };
+use smithay::wayland::drm_syncobj::{DrmSyncobjHandler, DrmSyncobjState};
 use smithay::wayland::fractional_scale::FractionalScaleHandler;
 use smithay::wayland::idle_inhibit::IdleInhibitHandler;
 use smithay::wayland::idle_notify::{IdleNotifierHandler, IdleNotifierState};
@@ -699,6 +700,12 @@ impl DrmLeaseHandler for State {
             .get_device_from_node(node)
             .unwrap()
             .remove_lease(lease_id);
+    }
+}
+
+impl DrmSyncobjHandler for State {
+    fn drm_syncobj_state(&mut self) -> Option<&mut DrmSyncobjState> {
+        self.niri.syncobj_state.as_mut()
     }
 }
 
